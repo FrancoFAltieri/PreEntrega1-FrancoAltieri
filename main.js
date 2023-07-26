9d55954d839ed2fd3fa97eb1fdc9a0778185acd3
FrancoFAltieri <franco.f.altieri@hotmail.com>
2023-07-25 23:04:23 -0300

    PreEntrega1+FrancoAltieri

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+
+let rapero = prompt("Cual es el mejor rapero?");
+
+while (rapero != "ESC") {
+  switch (rapero) {
+    case "Nas":
+      alert("Nas es el mejor");
+      break;
+    case "Jayz":
+      alert("JayZ es muy bueno");
+      break;
+      case "Eminem":
+      alert("Eminem es bueno");
+      break;
+    default:
+      alert("Dejanos tu opinión");
+      break;
+  }
+  rapero = prompt("Cual es el mejor rapero?");    }
+
+
+
+
+  
